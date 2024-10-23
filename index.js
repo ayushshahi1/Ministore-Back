68d5ad12f2fee9c58a733f75aaa58ef1315c49b3
HunterrBoyy <shahiaaayus@gmail.com>
2024-10-23 13:23:28 +0545

    ch

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const port = 5000;
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const fileUpload = require('express-fileupload');
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const fileUpload = require("express-fileupload");
+//kjkj
 
+mongoose.set("strictQuery", false);
 
-mongoose.set('strictQuery',false)
-
-mongoose.connect('mongodb+srv://hunterboy:ayush1998@cluster0.fxwwuen.mongodb.net/MiniStore').then((res) => {
-  app.listen(port, () =>{
-    console.log('listening', port)
+mongoose
+  .connect(
+    "mongodb+srv://hunterboy:ayush1998@cluster0.fxwwuen.mongodb.net/MiniStore"
+  )
+  .then((res) => {
+    app.listen(port, () => {
+      console.log("listening", port);
+    });
   })
-}).catch((err)=>{
-  console.log(err)
-})
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(cors());
-app.use(express.json())
-app.use('/uploads', express.static('uploads'))
-app.use(express.urlencoded({extended: true}))
-app.use(fileUpload({
-  limits: { fileSize: 5 * 1024 *1024 },
-  abortOnLimit: true,
-}));
-
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    limits: { fileSize: 5 * 1024 * 1024 },
+    abortOnLimit: true,
+  })
+);
 
 app.use(authRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
 
-app.use((req,res) => {
-  return res.status(404).json('not found')
-})
-
-
-
+app.use((req, res) => {
+  return res.status(404).json("not found");
+});
